b356e44a3f731f94e7b800325004e869b8eb24c0
ISH k pro <princeishimwe754@gmail.com>
2025-10-13 21:39:26 +0200

    Wrapped the entire app into SafeScreen component

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,5 +1,15 @@
 import { Stack } from "expo-router";
+import SafeScreen from "@/components/SafeScreen";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <SafeScreen>
+        <Stack screenOptions={{ headerShown: false }} />;
+      </SafeScreen>
+      <StatusBar barStyle={"dark-content"} />
+    </SafeAreaProvider>
+  );
 }
