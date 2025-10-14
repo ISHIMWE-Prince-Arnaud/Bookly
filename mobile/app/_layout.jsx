e49b1aa7aa27665fb10f7963461d92ccd1a10751
ISH k pro <princeishimwe754@gmail.com>
2025-10-14 09:52:28 +0200

    Finished working on login screen

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,15 +1,23 @@
 import { Stack } from "expo-router";
-import SafeScreen from "@/components/SafeScreen";
+import SafeScreen from "@/components/SafeScreen.jsx";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StatusBar } from "react-native";
+import { useEffect } from "react";
+import { useAuthStore } from "../store/authStore.js";
 
 export default function RootLayout() {
+  const checkAuth = useAuthStore((state) => state.checkAuth);
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
   return (
     <SafeAreaProvider>
       <SafeScreen>
-        <Stack screenOptions={{ headerShown: false }} />;
+        <Stack screenOptions={{ headerShown: false }} />
       </SafeScreen>
-      <StatusBar barStyle={"dark-content"} />
+      <StatusBar barStyle="dark-content" />
     </SafeAreaProvider>
   );
 }
--- mobile/components/SafeScreen.jsx
+++ mobile/components/SafeScreen.jsx
@@ -1,14 +1,20 @@
-import { View } from 'react-native'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import {  COLORS } from '@/constants/colors.js'
+import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import COLORS from "@/constants/colors.js";
 
-const SafeScreen = ({children}) => {
-          const insets = useSafeAreaInsets();
+const SafeScreen = ({ children }) => {
+  const insets = useSafeAreaInsets();
   return (
-    <View style={{paddingTop: insets.top, flex: 1, backgroundColor: COLORS.background, paddingBottom: insets.bottom}}>
-          {children}
+    <View
+      style={{
+        paddingTop: insets.top,
+        flex: 1,
+        backgroundColor: COLORS.background,
+        paddingBottom: insets.bottom,
+      }}>
+      {children}
     </View>
-  )
-}
+  );
+};
 
-export default SafeScreen
+export default SafeScreen;
