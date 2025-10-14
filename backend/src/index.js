1e7e5d9edaffd71c2d4c2c92cccd91f05a6624c6
ISH k pro <princeishimwe754@gmail.com>
2025-10-14 17:05:36 +0200

    Added cron job

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.routes.js";
+import job from "./lib/cron.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+job.start();
 
 app.use(express.json());
 
